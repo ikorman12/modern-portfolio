b850da6427115877466fe8a440c9d34955b993e7
Igor Korman <ikorman12@gmail.com>
2022-12-09 01:51:20 -0500

    succesffuly got the react versiom to work

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <>
+      <header>This is my portfolio using React</header>
+      <main>projects go here</main>
+      <footer>This is the footer</footer>
+    </>
+  );
+}
